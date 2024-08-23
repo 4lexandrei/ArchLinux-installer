e80103fd886b18dfa35c638623489cbe1077b517
alexandrei <alexandreijohnvecinal@gmail.com>
2024-08-23 21:06:11 +0200

    Added BIOS support

--- scripts/bootloader.sh
+++ scripts/bootloader.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+source ./ArchLinux-installer/config.sh
+
 # Grub installation
 echo "Installing GRUB bootloader..."
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+
+if [ -d /sys/firmware/efi ]; then
+    grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB --recheck
+else
+    grub-install --target=i386-pc --recheck "/dev/$DISK_ID"
+fi
 
 # Generate configuration files
 grub-mkconfig -o /boot/grub/grub.cfg
